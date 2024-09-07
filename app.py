b94bdfddee9695b826b8bb589478f60af5dea5fe
Ekaterina Kuzmenko <katya.kuzmen@yandex.ru>
2024-09-07 13:26:00 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
-    
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Кузьменко Екатерина Алексеевна, Лабораторная 1</title>
+    </head> 
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Кузьменко Екатерина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
