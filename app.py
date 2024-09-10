ca362d7a5391e7e5f57e23aa18f5255687a984c6
Ekaterina Kuzmenko <katya.kuzmen@yandex.ru>
2024-09-10 15:15:37 +0700

    Меню

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
-from flask import Flask 
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
 <!doctype html>
 <html>
     <head>
-        <title>Кузьменко Екатерина Алексеевна, Лабораторная 1</title>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
     </head> 
     <body>
         <header>
-            НГТУ, ФБ, Лабораторная работа 1
+            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
         </header>
+        <nav>
+            <ul>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </ul>
+        </nav>
+        <footer>
+            &copy; Кузьменко Екатерина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
 
-        <h1>web-сервер на flask</h1>
-
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head> 
+    <body>
+        <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
         <footer>
             &copy; Кузьменко Екатерина, ФБИ-23, 3 курс, 2024
         </footer>
