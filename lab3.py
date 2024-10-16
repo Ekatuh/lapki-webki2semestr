4dc64d3e6975c48d416aa14c601d4bb64bab89ed
Ekaterina Kuzmenko <katya.kuzmen@yandex.ru>
2024-10-16 22:10:29 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template, render_template_string, abort, request, make_response
+from flask import Blueprint, redirect, url_for, render_template, render_template_string, abort, request, make_response, session
 
 lab3 = Blueprint('lab3', __name__, static_folder='static')
 
@@ -45,3 +45,30 @@ def form1():
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors, errors2=errors2)
 
 
+@lab3.route('/lab3/order')
+def order():
+    return render_template ('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template ('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', 0)
+    return render_template ('lab3/success.html', price=price)
