c99e75a1d3029b0f05680ae38cbd33dc78bf9860
Екатерина Кузьменко <katya.kuzmen@yandex.ru>
2024-12-22 21:12:30 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, render_template_string, abort, request, session
-import os 
+import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -9,10 +12,26 @@ from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
 from rgz import rgz
-app = Flask(__name__, static_folder='static')
 
+
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'ekatushka'
+    db_user = "postgres"
+    db_password = "katya9056"
+    host_ip = "127.0.0.1"
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "ekaterina_kuzmenko_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
+
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -29,8 +48,7 @@ app.register_blueprint(rgz)
 def start():
     return redirect("/menu", code=302)
 
-
 @app.route("/menu")
 def menu():
-        return render_template('menu.html')
+    return render_template('menu.html')
 
--- lab8.py
+++ lab8.py
@@ -4,7 +4,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
-from os import path
+
 from datetime import datetime
 
 
@@ -14,3 +14,5 @@ lab8 = Blueprint('lab8', __name__, static_folder='static')
 def lab():
   username = session.get('login', '')
   return render_template('lab8/lab8.html', login=session.get('login'), username=username)
+
+
